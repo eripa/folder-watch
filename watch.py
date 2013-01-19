2b66852a4e063c888fa75985e3ea223a241f7a02
Eric Ripa <eric.ripa@me.com>
2013-01-19 12:37:59 +0100

    Added time stamps

--- watch.py
+++ watch.py
@@ -5,6 +5,7 @@ import os
 import sys
 import pickle
 import smtplib
+import datetime
 
 #monitor_path = os.path.join(os.getenv('HOME'), 'Pictures', 'Aperture Library.aplibrary', 'Masters')
 
@@ -44,11 +45,14 @@ def getDataFromDisk(pickle_file):
 		with open(pickle_file, 'r') as pfile:
 			return pickle.load(pfile)
 	else:
-		return []
+		return {'run' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
+				'data': []}
 
 def writeDataToDisk(pickle_file, data):
+	datadict = {'run' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
+				'data': data}
 	with open(pickle_file, 'w') as pickle_db:
-		pickle.dump(data, pickle_db)
+		pickle.dump(datadict, pickle_db)
 
 def scanPath(dir):
 	ret = []
@@ -60,8 +64,8 @@ def compareData(stored_data, data):
 	return list(set(stored_data[1:]).symmetric_difference(data[1:]))
 	return [(i,j) for i,j in zip(stored_data,data) if i!=j]
 
-def generateMessage(data):
-	msg = 'The following files have been removed:\n'
+def generateMessage(data, runtime):
+	msg = 'The following files have been removed since last run (%s):\n' % (runtime)
 	for file in data:
 		msg += file + '\n'
 	return msg
@@ -72,19 +76,19 @@ def main():
 		checkInput(folder)
 		datafile = os.path.join(os.path.dirname(sys.argv[0]), 'data.db')
 		stored_data = getDataFromDisk(datafile)
-		data = scanPath(folder)
-		difference = compareData(stored_data, data)
-		print 'amount of files last run:', len(stored_data)
-		print 'amount of files this run:', len(data)
-		print 'difference:', len(difference)
-		if len(stored_data) > len(data):
+		newdata = scanPath(folder)
+		difference = compareData(stored_data['data'], newdata)
+		print 'amount of files last run (%s): %s' % (stored_data['run'], len(stored_data['data']))
+		print 'amount of files this run: %s' % (len(newdata))
+		print 'difference: %s' % (len(difference))
+		if len(stored_data['data']) > len(newdata):
 			subject = 'Removed files from %s' % (folder)
-			message = generateMessage(difference)
+			message = generateMessage(difference, stored_data['run'])
 			print message
 			sendGmail(from_address, from_address, subject, message)
 		else:
 			print 'Only new files or same amount, nothing to do.'
-		writeDataToDisk(datafile, data)
+		writeDataToDisk(datafile, newdata)
 
 	except IOError as e:
 		err_str = "I/O error({0}): %s: {1}".format(e.errno, e.strerror) % (e.filename)
