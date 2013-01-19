f0691b19a43fffc0f71d9565dc6727c085d012d6
Eric Ripa <eric.ripa@me.com>
2013-01-19 11:58:58 +0100

    pretty much done.. working version. Could perhaps need some timestamps and more intelligence

--- watch.py
+++ watch.py
@@ -2,61 +2,98 @@
 #
 
 import os
-import datastore
-from datastore.impl.filesystem import FileSystemDatastore
 import sys
-import datetime
+import pickle
+import smtplib
+
+#monitor_path = os.path.join(os.getenv('HOME'), 'Pictures', 'Aperture Library.aplibrary', 'Masters')
+
+from_address = 'you@foo.bar'
+from_address = 'you@foo.bar'
+gmail_user = "you@foo.bar"
+gmail_pwd = "fooBar1234"
 
 
-#import pickle
+def sendGmail(from_address, to_address, subject, message):
+	FROM = from_address
+	TO = [to_address] #must be a list
+	SUBJECT = subject
+	TEXT = message
 
-# pickle.dump(itemlist, outfile)
-# To read it back:
-
-# itemlist = pickle.load(infile)
-
-# some global settings
-monitor_path = os.path.join(os.getenv('HOME'), 'Pictures', 'iPhoto Library', 'Masters')
-history_to_keep = 10
+	# Prepare actual message
+	message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+	""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+	try:
+		#server = smtplib.SMTP(SERVER) 
+		server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+		server.ehlo()
+		server.starttls()
+		server.login(gmail_user, gmail_pwd)
+		server.sendmail(FROM, TO, message)
+		#server.quit()
+		server.close()
+		print 'successfully sent the mail'
+	except:
+		print "failed to send mail"
 
 def checkInput(dir):
-	if not os.path.isdir(monitor_path): raise IOError('Directory '+ dir +' does not seem to exist')
+	if not os.path.isdir(dir): raise IOError('Directory '+ dir +' does not seem to exist')
 
-def initializeDS(ds, date):
-	initkey = datastore.Key(0)
-	if not ds.contains(initkey):
-		ds.put(initkey, "Database created "+ date.strftime("%Y%m%d-%H%M"))
-		print ds.get(initkey)
-		return True
+def getDataFromDisk(pickle_file):
+	if os.path.isfile(pickle_file):
+		with open(pickle_file, 'r') as pfile:
+			return pickle.load(pfile)
 	else:
-		return True
+		return []
 
-def getNextIndex(ds):
-	i = 0
-	next_key = datastore.Key(i)
-	while ds.contains(next_key):
-		i += 1
-		next_key = datastore.Key(i)
-	last_key = datastore.Key(i - 1)
-	return last_key, next_key
+def writeDataToDisk(pickle_file, data):
+	with open(pickle_file, 'w') as pickle_db:
+		pickle.dump(data, pickle_db)
+
+def scanPath(dir):
+	ret = []
+	for path, dirs, files in os.walk(dir):
+		ret += files
+	return sorted(ret)
+
+def compareData(stored_data, data):
+	return list(set(stored_data[1:]).symmetric_difference(data[1:]))
+	return [(i,j) for i,j in zip(stored_data,data) if i!=j]
+
+def generateMessage(data):
+	msg = 'The following files have been removed:\n'
+	for file in data:
+		msg += file + '\n'
+	return msg
 
 def main():
+	folder = sys.argv[1]
 	try:
-		checkInput(monitor_path)
-
-		date = datetime.datetime.now()
-		ds = FileSystemDatastore('datastore')
-		
-		initializeDS(ds, date)
-		last_key, next_key = getNextIndex(ds)
-		print "Last written: %s" % (last_key)
-		print "Setting index %s" % (next_key)
-		ds.put(next_key, ['bla', 'bla2', 'bla3'])
+		checkInput(folder)
+		datafile = os.path.join(os.path.dirname(sys.argv[0]), 'data.db')
+		stored_data = getDataFromDisk(datafile)
+		data = scanPath(folder)
+		difference = compareData(stored_data, data)
+		print 'amount of files last run:', len(stored_data)
+		print 'amount of files this run:', len(data)
+		print 'difference:', len(difference)
+		if len(stored_data) > len(data):
+			subject = 'Removed files from %s' % (folder)
+			message = generateMessage(difference)
+			print message
+			sendGmail(from_address, from_address, subject, message)
+		else:
+			print 'Only new files or same amount, nothing to do.'
+		writeDataToDisk(datafile, data)
 
 	except IOError as e:
-		sys.exit(e.message)
+		err_str = "I/O error({0}): %s: {1}".format(e.errno, e.strerror) % (e.filename)
+		sys.exit(err_str)
 	except KeyboardInterrupt:
 		print 'Aborting..'
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+		raise
 	sys.exit(0)
 
 
